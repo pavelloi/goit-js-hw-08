a75d79beeccde6030662869b98e8581909d52691
pavelloi <pashaloy95@gmail.com>
2022-01-30 20:38:05 +0200

    1st task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCAL_TIME = 'videoplayer-current-time';
+const localTime = localStorage.getItem(LOCAL_TIME);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+if (localTime) {
+  player.setCurrentTime(localTime);
+}
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
+function timeUpdate(e) {
+  const time = e.seconds;
+  localStorage.setItem(LOCAL_TIME, time);
+}
