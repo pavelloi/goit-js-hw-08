7a5deb14efe52a99b8ee6847201e3eaee92b5f4c
pavelloi <pashaloy95@gmail.com>
2022-01-30 21:12:37 +0200

    updated tasks 2 and 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCAL_STORAGE = 'feedback-form-state';
+let items = {};
+
+form.addEventListener('submit', formSubmit);
+form.addEventListener('input', throttle(textAreaInput, 500));
+populateForm();
+
+function textAreaInput(event) {
+    items[event.target.name] = event.target.value;
+    localStorage.setItem(LOCAL_STORAGE, JSON.stringify(items));
+};
+
+function formSubmit(event) {
+    event.preventDefault();
+
+    if (form.email.value === '' || form.message.value === '') {
+        alert('All fields are required!');
+    } else {
+        console.log(items);
+        event.currentTarget.reset();
+
+        localStorage.removeItem(LOCAL_STORAGE);
+        items = {};
+
+        
+    };
+};
+
+function populateForm() {
+    const savedObject = JSON.parse(localStorage.getItem(LOCAL_STORAGE));
+   
+    for (const key in savedObject) {
+        if (key) {
+            form[key].value = savedObject[key];
+            items = savedObject;
+        }; 
+    };
+};
